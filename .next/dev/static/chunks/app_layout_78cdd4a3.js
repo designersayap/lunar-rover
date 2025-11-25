70f116bf439fde159631ed1a3ab79114317a4314
daruarya <pradanadaru@gmail.com>
2025-11-25 13:21:30 +0700

    Chore: Update .gitignore and untrack .next directory

--- .next/dev/static/chunks/app_layout_78cdd4a3.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_67ea7b8c._.css"
-],
-    source: "dynamic"
-});
