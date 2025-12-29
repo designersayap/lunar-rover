a46acfc7cc9c207aa6d149fdf29de3d5936083b6
daruarya <pradanadaru@gmail.com>
2025-12-29 16:32:27 +0700

    Cleanup: Remove obsolete staging pages and preview artifacts

---
+++ app/staging/berkah-ekonomi/layout.js
@@ -0,0 +1,38 @@
+
+
+export const metadata = {
+  title: "Lunar Staging",
+  description: "Staging Preview",
+  
+  
+  
+  openGraph: {
+    
+    
+    
+  },
+};
+
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
+export default function StagingLayout({ children }) {
+  return (
+    <>
+      {children}
+      
+      {/* Analytics Scripts */}
+      
+
+      
+
+      
+
+      
+    </>
+  );
+}
